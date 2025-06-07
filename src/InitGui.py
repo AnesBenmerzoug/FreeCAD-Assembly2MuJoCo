49d24189ecde6239c8c856e18eacdb54b87aaa5a
Anes Benmerzoug <anes.benmerzoug@gmail.com>
2025-06-07 12:12:47 +0200

    Define constants directly in InitGui.py

--- src/InitGui.py
+++ src/InitGui.py
@@ -1,16 +1,24 @@
 import os
+from pathlib import Path
 
 import FreeCADGui as Gui
 
-from src.constants import ICONS_DIR, WORKBENCH_ICON_FILE, WORKBENCH_MENU_NAME
 from src.commands import get_all_commands
 from src.utils.helpers import log_message
 
 
+ROOT_DIR = Path(__file__).parents[1].resolve()
+SRC_DIR = ROOT_DIR / "src"
+RESOURCES_DIR = SRC_DIR / "resources"
+ICONS_DIR = RESOURCES_DIR / "icons"
+WORKBENCH_ICON_FILE = os.fspath(ICONS_DIR / "assembly-to-mujoco-icon.svg")
+WORKBENCH_MENU_NAME = "Assembly To MuJoCo"
+
+
 class AssemblyExportToMuJoCoWorkbench(Gui.Workbench):
-    MenuText = "Assembly To MuJoCo"
+    MenuText = WORKBENCH_MENU_NAME
     Tooltip = "Export FreeCAD assemblies to MuJoCo"
-    Icon = os.fspath(WORKBENCH_ICON_FILE)
+    Icon = WORKBENCH_ICON_FILE
 
     def Initialize(self):
         log_message("Initializing workbench")
@@ -19,8 +27,8 @@ class AssemblyExportToMuJoCoWorkbench(Gui.Workbench):
             command_names.append(command_name)
             Gui.addCommand(command_name, command_cls())
         log_message(f"Adding {len(command_names)} commands")
-        self.appendToolbar(f"{WORKBENCH_MENU_NAME} Tools", command_names)
-        self.appendMenu(f"{WORKBENCH_MENU_NAME} Tools", command_names)
+        self.appendToolbar(WORKBENCH_MENU_NAME, command_names)
+        self.appendMenu(WORKBENCH_MENU_NAME, command_names)
         log_message(f"Using icons in {ICONS_DIR}")
         Gui.addIconPath(os.fspath(ICONS_DIR))
 
